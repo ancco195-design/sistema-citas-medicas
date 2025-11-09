6a2be2307b805fb7128ac4fff5b0c8a879607598
juan <ancco195@gmail.com>
2025-11-08 21:13:51 -0600

    Configuración completa de Firebase y AngularFire - Proyecto funcionando

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD6KwMsxmlAKvCV4n3BVKcJQpBSWxX9PA4",
+    authDomain: "sistema-citas-medicas-365f8.firebaseapp.com",
+    projectId: "sistema-citas-medicas-365f8",
+    storageBucket: "sistema-citas-medicas-365f8.firebasestorage.app",
+    messagingSenderId: "755000021757",
+    appId: "1:755000021757:web:fd50ea8971329a8d2a6494"
+  }
+};
